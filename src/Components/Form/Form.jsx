ae0bf8176154581ecaddedd703921fc873a81b64
larissa-azuma <azumalinda29@gmal.com>
2024-04-04 06:16:44 +0100

    In Progress

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { TextField } from '@mui/material'
+import { TextField, Typography } from '@mui/material'
 
 function Form() {
   return (
     <div>
+      <Typography variant='h3' alighn='center '>Next Destination</Typography>
       <TextField id="outlined-basic "label="Pick-up Point  "  variant="outlined" /> <br/>
       <TextField id="outlined-basic " label="Select the Departure Date" variant="outlined" /><br/>
       <TextField id="outlined-basic " label="Pick-up Point" variant="outlined" />
