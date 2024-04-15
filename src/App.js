f4bc76ab128626cbd13553fd13dd06a8bfd48146
Gerald1702 <ampadugerald17@gmail.com>
2024-04-15 10:26:06 +0000

    ratings page almost completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SquareCorners from './Pages/Home/home'
+
+const App = () => {
+  return (
+    <div>
+     <SquareCorners/>
+    </div>
+  )
+}
+
+export default App
