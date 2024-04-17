a720f8e1b994746d5c035a4b566d2c9b2db8aa7f
Gerald1702 <ampadugerald17@gmail.com>
2024-04-17 15:38:42 +0000

    done with Largent page

--- src/Components/l'argent/Largent.jsx
+++ src/Components/l'argent/Largent.jsx
@@ -1,70 +1,101 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Card, TextField, Button } from '@mui/material';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Card,
+  TextField,
+  Button,
+} from "@mui/material";
 import Project1 from "../../assets/images/Picture1.png";
 import Project2 from "../../assets/images/Picture2.png";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-function Largent () {
+function Largent() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div>
       {/* Header */}
-      <AppBar position="static" sx={{bgcolor:'#001023'}}>
+      <AppBar position="static" sx={{ bgcolor: "black" }}>
+        <ArrowBackIosNewOutlinedIcon sx={{ marginTop: "20px" }} />
 
         <Toolbar>
-          <Typography variant="h3" sx={{className:'center'}} > 
-          Ajoutez de l'argent
+          <Typography
+            variant="h6"
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            Ajoutez de l'argent
           </Typography>
         </Toolbar>
       </AppBar>
-             
-      {/* Money Card */}
-      <Card>
-      <img src={Project1}  alt='Project1'></img>
-      <img src={Project2}  alt='Project2'></img>
 
-      
-      </Card>
+      {/* Money Card */}
+      <Slider {...settings}>
+        <div>
+          <Card sx={{ marginTop: "100px", marginBottom: "100px" }}>
+            <img src={Project1} alt="Project1" />
+          </Card>
+        </div>
+        <div>
+          <Card sx={{ marginTop: "100px", marginBottom: "100px" }}>
+            <img src={Project2} alt="Project2" />
+          </Card>
+        </div>
+      </Slider>
 
       {/* Input Fields */}
-      <div className='h4'>Ajoutez le montant</div>
+      <div sx={{ fontFamily: "jost" }}>Ajoutez le montant</div>
+      <br />
       <TextField
         label="Entrez le montant à déposer"
         variant="outlined"
         fullWidth
-        // Add other props as needed
-      /> 
-      <hr/>
-      <div className='h5'>Le numéro de téléphone pour la demande</div>
+        sx={{ marginBottom: "100px" }}
+      />
+
+      <div sx={{ marginBottom: "50px" }}>
+        Le numéro de téléphone pour la demande
+      </div>
+      <br />
       <TextField
         label="Entrez le montant à téléphone"
         variant="outlined"
         fullWidth
-        // Add other props as needed
+        sx={{ marginBottom: "60px" }}
       />
 
       {/* Order Button */}
       <Button
         variant="contained"
         sx={{
-          borderBottomLeftRadius: "24px",
-          borderBottomRightRadius: "24px",
           fontWeight: "bold",
           textTransform: "none",
-          backgroundColor: "#001023",
+          backgroundColor: "black",
           width: "100%",
           height: "40px",
           top: "10px",
           Left: "60px",
 
           "&:hover": {
-            backgroundColor: "#001023",
+            backgroundColor: "black",
             boxShadow: "none",
           },
         }}
       >
-       Retour à l’accueil
-      </Button> 
+        Commnade
+      </Button>
     </div>
   );
-};
+}
 
-export default Largent ;
+export default Largent;
